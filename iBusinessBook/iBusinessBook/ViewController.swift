3f8d8c26b9eea0d35071f16bb19a0e8bd94af1e6
KatherinChuco <u201422865@upc.edu.pe>
2018-05-20 15:28:55 -0500

    first commit

---
+++ iBusinessBook/iBusinessBook/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  iBusinessBook
+//
+//  Created by Jacqueline Altamirano on 20/05/18.
+//  Copyright © 2018 ACME. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
